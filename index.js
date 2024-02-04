a24ce495e85048acd9f0bf322bde88535506e4d4
jonathankim345 <jonkim345@gmail.com>
2024-02-03 23:31:46 -0500

    First push

---
+++ index.js
@@ -0,0 +1,81 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { typeDefs } from './schema.js'
+import db from './_db.js'
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id);
+        },
+        authors() {
+            return db.authors
+        },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id);
+        },
+        reviews() {
+            return db.reviews
+        },
+        review(_, args) {
+            return db.reviews.find((review) => review.id === args.id);
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
+        }
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            db.games = db.games.filter((g) => g.id != args.id)
+            return db.games
+        },
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game)
+
+            return game
+        },
+        updateGame(_, args) {
+            db.games = db.games.map((g) => {
+                if (g.id === args.id) {
+                    return { ...g, ...args.edits }
+                }
+
+                return g
+            })
+            return db.games.find((g) => g.id === args.id)
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+})
+
+console.log('Server ready at port', 4000)
